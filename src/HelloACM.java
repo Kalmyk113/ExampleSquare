2fa63ac92d21ba3243096bb1044f7a9125069432
12045950 <john_michael_callanta@dlsu.edu.ph>
2023-05-12 15:54:38 +0800

    from lab

--- src/HelloACM.java
+++ src/HelloACM.java
@@ -1,6 +1,8 @@
 import acm.graphics.*;
 import acm.program.*;
 
+import java.awt.*;
+
 
 public class HelloACM extends GraphicsProgram {
 
@@ -9,11 +11,14 @@ public class HelloACM extends GraphicsProgram {
 //    }
 
     public void run(){
-        GLabel label = new GLabel("Hello World");
-        add(label, 100, 100);
-
         GRect rect = new GRect(200, 300);
+        rect.setColor(Color.blue);
+        rect.setFilled(true);
         add(rect,50,50);
+
+        GLabel label = new GLabel("Hello World");
+        label.setColor(Color.white);
+        add(label, 100, 100);
     }
 
     public static void main(String[] args){
